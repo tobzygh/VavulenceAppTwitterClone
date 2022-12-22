840795566873401b4fb27ddad5e2c6f2a5dd7202
tobzygh <tobzyunlimited@gmail.com>
2022-12-22 19:01:37 +0100

    Chapter 1

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+  public class UsersController : BaseApiController
+  {
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUserId(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        return user;
+    }
+  }
+}
